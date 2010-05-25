766eafae9c4b602a27842523509290d9beeddf82
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2010-05-25 00:16:11 -0700

    Change trie to use a map for sub tries.

--- src/Trie.hs
+++ src/Trie.hs
@@ -1,37 +1,19 @@
 module Trie where
 
-import Control.Monad(msum)
+import qualified Data.Map as Map
+import Data.Maybe(fromMaybe)
 
-data Trie a b = Sub [(a, Trie a b)] (Maybe b)
+data Trie a b = Sub (Map.Map a (Trie a b)) (Maybe b)
                 deriving Show
 
 empty :: Trie a b
-empty = Sub [] Nothing
+empty = Sub Map.empty Nothing
 
-lookup :: Eq a => [a] -> Trie a b -> Maybe b
+lookup :: Ord a => [a] -> Trie a b -> Maybe b
 lookup [] (Sub _ b)       = b
-lookup (k:ks) (Sub as _)  = Trie.lookup ks =<< Prelude.lookup k as
+lookup (k:ks) (Sub as _)  = Trie.lookup ks =<< Map.lookup k as
 
-lookupPrefix :: Eq a => [a] -> Trie a b -> Maybe ([a],b)
-lookupPrefix _      (Sub _ (Just b)) = Just ([],b)
-lookupPrefix (k:ks) (Sub ts _)       = do t <- Prelude.lookup k ts
-                                          (as,v) <- lookupPrefix ks t
-                                          return ((k:as),v)
-lookupPrefix []     (Sub _ _)        = Nothing
-
-
-insert :: Ord a => [a] -> (Maybe b -> b) -> Trie a b -> Trie a b
+insert :: (Ord a) => [a] -> (Maybe b -> b) -> Trie a b -> Trie a b
 insert [] f (Sub as b)     = Sub as (Just (f b))
-insert (k:ks) f (Sub as b) = Sub (upd as) b
-
-  where upd ((k1,t):ss)
-          | k1 < k    = (k1,t) : upd ss
-          | k1 == k   = (k1, insert ks f t) : ss
-        upd ss = (k, insert ks f empty) : ss
-
-find :: (b -> Maybe c) -> Trie a b -> Maybe c
-find p (Sub ts b) = msum ((p =<< b) : map (find p . snd) ts)
-
-
-
-
+insert (k:ks) f (Sub as b) = Sub (Map.alter upd k as) b
+  where upd j = Just $ insert ks f $ fromMaybe empty j
